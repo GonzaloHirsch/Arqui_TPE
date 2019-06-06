649132bd3a807a582afba56571c475103ad9e387
GonzaloHirsch <40722245+GonzaloHirsch@users.noreply.github.com>
2019-06-05 21:59:08 -0300

    Agregue el test que hicimos

---
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/asm/libasm.asm
@@ -0,0 +1,42 @@
+GLOBAL cpuVendor
+GLOBAL getSeconds
+
+section .text
+	
+cpuVendor:
+	push rbp
+	mov rbp, rsp
+
+	push rbx
+
+	mov rax, 0
+	cpuid
+
+
+	mov [rdi], ebx
+	mov [rdi + 4], edx
+	mov [rdi + 8], ecx
+
+	mov byte [rdi+13], 0
+
+	mov rax, rdi
+
+	pop rbx
+
+	mov rsp, rbp
+	pop rbp
+	ret
+
+getSeconds:
+	push rbp
+	mov rbp, rsp
+
+	mov al, 0
+	out 70h, al
+	in ax, 71h
+
+	mov eax, 30h
+
+	mov rsp, rbp
+	pop rbp
+	ret
