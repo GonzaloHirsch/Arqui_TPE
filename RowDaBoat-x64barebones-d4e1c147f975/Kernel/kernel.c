9b012e98a555883fce3ec43baff94245cac0f3e8
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-06-08 13:00:27 -0300

    Minor change

--- RowDaBoat-x64barebones-d4e1c147f975/Kernel/kernel.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Kernel/kernel.c
@@ -97,7 +97,7 @@ int main()
 	ncNewline();
 	ncPrint("Arquitectura de Computadoras");
 	ncNewline();
-	for (int i = 0; i < 1000000000; i++){
+	for (int i = 0; i < 2000000000; i++){
 		if ("sdfgsdf" == "dfghsdf"){
 
 		}
