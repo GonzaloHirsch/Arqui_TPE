3bda5eaa4346833d9eaa60a0a7189d832bc728db
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-06-09 18:55:25 -0300

    Initialized branch

---
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/include/shell.h
@@ -0,0 +1,17 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdint.h>
+#include <naiveConsole.h>
+
+// void ncPrint(const char * string);
+// void ncPrintChar(char character);
+// void ncNewline();
+// void ncPrintDec(uint64_t value);
+// void ncPrintHex(uint64_t value);
+// void ncPrintBin(uint64_t value);
+// void ncPrintBase(uint64_t value, uint32_t base);
+// void ncClear();
+// void ncPrintOnAddress(char * address, char * text);
+
+#endif
---
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/src/shell.c
@@ -0,0 +1,23 @@
+#include <shell.h>
+
+const char * commandMessages[] = {"help - Show available commands and their use", 
+								"snake - Initializes the Snake game", 
+								"shutdown - Shuts down and exits the system",
+								"verify - Runs verification routines for system exceptions",
+								"time - Displays system date and time"};
+
+const char * commands[] = {"help", "snake", "shutdown", "verify", "time", "beep"};
+
+
+
+void init_shell(void){
+
+}
+
+void display_welcome_message(){
+
+}
+
+void display_help(){
+	
+}
