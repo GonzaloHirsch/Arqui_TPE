e4afc1eec43f7c73dc19e09b08546d689bb4bb89
ig-rib <iribas@itba.edu.ar>
2019-06-09 00:14:27 +0000

    userland: syscall pasa de extern a estar en un .h

--- RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/include/interrupts.h
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/include/interrupts.h
@@ -8,6 +8,6 @@
 #include <stdint.h>
 
 //siguiendo el protocolo de funcion para 64 bit
-//void syscall(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, uint64_t r9);
+void syscall(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, uint64_t r9);
 
 #endif //ARQUI_TPE_INTERRUPTS_H
--- RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/src/sampleCodeModule.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/src/sampleCodeModule.c
@@ -6,8 +6,6 @@
 
 char * v = (char*)0xB8000 + 79 * 2;
 
-extern void syscall(uint64_t rdi, uint64_t rsi, uint64_t rdx, uint64_t rcx, uint64_t r8, uint64_t r9);
-
 static int var1 = 0;
 static int var2 = 0;
 
