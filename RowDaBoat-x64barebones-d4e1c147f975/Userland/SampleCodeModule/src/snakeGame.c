69ee656badc9e44a9e40b49688d9440ced3ba735
Gonza Hirsch <hirschgonzalo@yahoo.com.ar>
2019-06-15 16:12:39 -0300

    Auto stash before merge of "Feature_Snake" and "origin/Feature_Snake"

--- RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/src/snakeGame.c
+++ RowDaBoat-x64barebones-d4e1c147f975/Userland/SampleCodeModule/src/snakeGame.c
@@ -9,9 +9,17 @@ static int winner;
 static int snakeBody[400][2];
 static int snakeLength;
 
+static int totalTimeAlive = 0;
+static int partialTime = 0;
+static int relativeTime = 0;
+
+//La velocidad inicial es para la frecuencia mas baja
+static int velocity = 65356;
+static int velocityIncrements = 5000;
+
 void onExit();
 void nextFrame();
-void drawHeader();
+int abosulteTimeDifference(int before, int after);
 
 void resetVariables(){
   winner = 1;
@@ -30,11 +38,13 @@ int initSnakeGame(){
 
   initSnakeBody(startPos);
 
+  relativeTime = sys_time(SECONDS);
+
   nextFrame();
 
   onExit();
 
-  return winner;
+  return totalTimeAlive;
 }
 
 void updateInput(){
@@ -49,15 +59,36 @@ void updateInput(){
 }
 
 void Update(){
+  int auxTime = sys_time(SECONDS);
+  int delta = abosulteTimeDifference(relativeTime, auxTime);
+  relativeTime = auxTime;
+  totalTimeAlive += delta;
+  partialTime += delta;
+
+  if (partialTime >= 15){
+    growSnake();
+    sys_beep();
+    velocity -= velocityIncrements;
+    sys_over_clock(velocity);
+    partialTime = 0;
+  }
+
   updateInput();
   if(isSnakeColliding()){
     isRunning = 0;
     winner = 0;
   }
-  updateSnake(); 
+  updateSnake();
 }
 
-
+int abosulteTimeDifference(int before, int after){
+  int result = 0;
+  if (after < before){
+    result = 60 - before + after;
+  } else {
+    result = after - before;
+  }
+}
 
 void nextFrame(){
   _sti();
